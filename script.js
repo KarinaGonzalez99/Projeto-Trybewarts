b7c5bed49805336b89243d2d91cb2135b2a3bc82
Karina Gonzalez <karinagonzalez99@gmail.com>
2022-10-14 19:45:38 -0300

    lint

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 const emailconf = document.getElementById('Email');
 const passwordconf = document.getElementById('Password');
 const button = document.getElementById('botao');
-
 function checar() {
-    const mail = 'tryber@teste.com';
-    const senha = '123456';
-
-    if (emailconf.value === mail && passwordconf.value === senha) {
+  const mail = 'tryber@teste.com';
+  const senha = '123456';
+  if (emailconf.value === mail && passwordconf.value === senha) {
     window.alert('Olá, Tryber!');
   } else {
     window.alert('Email ou senha inválidos.');
   }
 }
-
 button.addEventListener('click', checar);
