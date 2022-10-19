24e7c9c68e39f0c5f37d802d1b59e7f8ca1b4b0f
Karina Gonzalez <karinagonzalez99@gmail.com>
2022-10-18 22:45:54 -0300

    requisito 1 ao 10 Karina e requisito 11 ao 19 Mariana

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-const emailconf = document.getElementById('Email'); // requisito 3
-const passwordconf = document.getElementById('Password'); // requisito 3
-const button = document.getElementById('botao'); // requisito 3
-function checar() { // requisito 3
-  const mail = 'tryber@teste.com'; // requisito 3
-  const senha = '123456'; // requisito 3
-  if (emailconf.value === mail && passwordconf.value === senha) { // requisito 3
-    window.alert('Olá, Tryber!'); // requisito 3
-  } else { // requisito 3
-    window.alert('Email ou senha inválidos.'); // requisito 3
+// requisito 3
+const emailconf = document.getElementById('Email');
+const passwordconf = document.getElementById('Password');
+const button = document.getElementById('botao');
+function checar() {
+  const mail = 'tryber@teste.com';
+  const senha = '123456';
+  if (emailconf.value === mail && passwordconf.value === senha) {
+    window.alert('Olá, Tryber!');
+  } else {
+    window.alert('Email ou senha inválidos.');
   }
 }
-button.addEventListener('click', checar); // requisito 3
+button.addEventListener('click', checar);
+// requisito 3
+
+// requisito 18
+const botaum = document.getElementById('submit-btn');
+const checkBox = document.getElementById('agreement');
+function enableButton() {
+  if (checkBox.checked) {
+    botaum.disabled = false;
+  } else {
+    botaum.disabled = true;
+  }
+}
+checkBox.addEventListener('click', enableButton);
+// requisito 18
+
+const counter = document.getElementById('counter');
+const textarea = document.getElementById('textarea');
+function contador() {
+  const countup = 500 - (textarea.value.length);
+  counter.innerHTML = countup;
+}
+textarea.addEventListener('keyup', contador);
